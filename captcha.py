e8faef84b9acdeccc1f96cf8c2650eca35604059
pan <panbanglanfeng@gmail.com>
2018-05-29 23:05:16 -0400

    reuse the image functions

--- captcha.py
+++ captcha.py
@@ -28,45 +28,21 @@ class Captcha:
 
         return image
 
-    def split_captcha_into_letters(self):
+    def split_all_captchas(self):
         captcha_image_files = glob.glob(os.path.join(self.captcha_image_folder, "*"))
         counts = {}
         for (i, captcha_img_file) in enumerate(captcha_image_files):
             print('[Info] processing image {}/{}'.format(i + 1, len(captcha_image_files)))
             file_name = os.path.basename(captcha_img_file)
             captcha_correct_text, file_type = os.path.splitext(file_name)
-
             if file_type != '.png':
                 continue
-
-            # enhance contrast
             image = cv2.imread(captcha_img_file)
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            gray = cv2.copyMakeBorder(gray, 8, 8, 8, 8, cv2.BORDER_REPLICATE)
-            thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-            contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            contours = contours[0] if imutils.is_cv2() else contours[1]
-
-            # split into four letters by bounding
-            letter_image_regions = []
-            for contour in contours:
-                (x, y, w, h) = cv2.boundingRect(contour)
-
-                if w / h > 1.25:
-                    half_width = int(w / 2)
-                    letter_image_regions.append((x, y, half_width, h))
-                    letter_image_regions.append((x + half_width, y, half_width, h))
-                else:
-                    letter_image_regions.append((x, y, w, h))
-
-            if len(letter_image_regions) != 4:
-                continue
-
-            letter_image_regions = sorted(letter_image_regions, key=lambda x: x[0])
+            letter_image_regions, image = self.split_captcha_into_letters(image)
 
             for letter_bounding_box, letter_text in zip(letter_image_regions, captcha_correct_text):
                 x, y, w, h = letter_bounding_box
-                letter_image = gray[y - 2:y + h + 2, x - 2:x + w + 2]
+                letter_image = image[y - 2:y + h + 2, x - 2:x + w + 2]
                 save_path = os.path.join(self.letter_folder, letter_text)
 
                 if not os.path.exists(save_path):
@@ -77,6 +53,29 @@ class Captcha:
                 cv2.imwrite(p, letter_image)
 
                 counts[letter_text] = count + 1
+
+    def split_captcha_into_letters(self, image):
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = cv2.copyMakeBorder(image, 8, 8, 8, 8, cv2.BORDER_REPLICATE)
+        thresh = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+        contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours = contours[0] if imutils.is_cv2() else contours[1]
+
+        letter_image_regions = []
+        for contour in contours:
+            (x, y, w, h) = cv2.boundingRect(contour)
+            if w / h > 1.25:
+                half_width = int(w / 2)
+                letter_image_regions.append((x, y, half_width, h))
+                letter_image_regions.append((x + half_width, y, half_width, h))
+            else:
+                letter_image_regions.append((x, y, w, h))
+
+        if len(letter_image_regions) != 4:
+            print('image is not containing four letters')
+            return [], image
+
+        return sorted(letter_image_regions, key=lambda x: x[0]), image
 
     def rename_captcha_imgs(self):
         with open(self.captcha_result_file) as r:
--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from captcha import Captcha
 from recognizer import Recognizer
 
 recognizer = Recognizer()
-recognizer.predict_model()
+recognizer.predict_model('./captcha_imgs/2A8H.png')
